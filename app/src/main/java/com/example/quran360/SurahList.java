ce7bfddba002c97706f4564954ba22b9349299c2
Adrees <rana.adrees5890@gmail.com>
2022-08-30 15:50:23 +0500

    CHanges made

--- app/src/main/java/com/example/quran360/SurahList.java
+++ app/src/main/java/com/example/quran360/SurahList.java
@@ -4,22 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SurahList extends AppCompatActivity {
+    EditText searchField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surah_list);
 
+        searchField=findViewById(R.id.searchField);
         DBHelper dbhelper=new DBHelper(this);
         ArrayList<String> SANList;
         SANList=dbhelper.displaySurahName("SurahNameU");
@@ -44,6 +51,23 @@ public class SurahList extends AppCompatActivity {
                 Intent intent=new Intent(SurahList.this,AyahList.class);
                 intent.putExtra("SurahNo", String.valueOf(i));
                 startActivity(intent);
+            }
+        });
+
+        searchField.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        Log.i("Text change",search)
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
